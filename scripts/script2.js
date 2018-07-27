7d26bddcfe62e3c2ec92a1cd05ca8cb4d5524b5c
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2018-07-27 14:55:17 +0200

    test

---
+++ scripts/script2.js
@@ -0,0 +1,20 @@
+let action = document.querySelector(".action")
+let time = document.querySelector('.time')
+let article = document.querySelector(".t_shirt_1")
+let number = 1
+
+action.addEventListener(
+  'click',
+  function() {
+    setInterval(decollage, 1000);
+  }, false);
+
+  function decollage() {
+    if(number > 1) {
+      time.innerHTML = (number -= 1)
+    } else {
+      time.innerHTML = 0
+      article.style.transform = "translateX(1010px) translateY(-410px) scale(0.08) rotate(360deg)"
+      article.style.transition = "all 0.4s"
+    }
+  }
